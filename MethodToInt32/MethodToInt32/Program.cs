1901190481e194616b595752cb348c27a932769c
Dell <Dell@DESKTOP-QKEQKKO>
2017-01-17 18:53:03 +0400

    Method ToInt32(byte a,byte b,byte c,byte d)

--- MethodToInt32/MethodToInt32/Program.cs
+++ MethodToInt32/MethodToInt32/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿g System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 namespace MethodToInt32
 {
     class Program
-    { public static int ToInt32(byte a,byte b,byte c,byte d)
+    {
+        public static int ToInt32(byte a, byte b, byte c, byte d)
         {
             string str = a.ToString() + b.ToString() + c.ToString() + d.ToString();
             return int.Parse(str);
-            
+
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(ToInt32(2,3,5,4));
+            Console.WriteLine(ToInt32(2, 3, 5, 4));
         }
     }
 }
--- MethodToInt32/MethodToInt32/Properties/AssemblyInfo.cs
+++ MethodToInt32/MethodToInt32/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("MethodToInt32")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("MethodToInt32")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a057d71-1255-42f7-938c-8893dff0b96e")]
+[assembly: Guid("898bd6eb-761b-492e-bbc6-2d43669ab828")]
 
 // Version information for an assembly consists of the following four values:
 //
