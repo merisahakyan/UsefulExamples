7d869a89ebf0863462d89771946c758cd0e45cb1
Dell <Dell@DESKTOP-QKEQKKO>
2017-01-17 18:51:15 +0400

    Method ToInt32(byte a,byte b,byte c,byte d)

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodToInt32
+{
+    class Program
+    { public static int ToInt32(byte a,byte b,byte c,byte d)
+        {
+            string str = a.ToString() + b.ToString() + c.ToString() + d.ToString();
+            return int.Parse(str);
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToInt32(2,3,5,4));
+        }
+    }
+}
