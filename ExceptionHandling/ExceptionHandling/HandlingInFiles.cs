2488797fcd8e63dc3a4fdbce0d3fb709cdaad3e0
Dell <Dell@DESKTOP-QKEQKKO>
2017-01-18 16:56:26 +0400

    Exception Handling with files

---
+++ ExceptionHandling/ExceptionHandling/HandlingInFiles.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    public class HandlingInFiles
+    {
+        public static void TxtReader(string path)
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string s = String.Empty;
+                    while ((s = r.ReadLine()) != null)
+                        Console.WriteLine(s);
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("DirectoryNotFoundException");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("FileNotFoundException");
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("PathTooLongException");
+            }
+            catch (EndOfStreamException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("EndofStreaException");
+            }
+
+        }
+
+        public static void TxtWriter(string path)
+        {
+            try
+            {
+                using (StreamWriter r = new StreamWriter(path))
+                {
+                    r.WriteLine(Console.ReadLine());
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("DirectoryNotFoundException");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("FileNotFoundException");
+                File.Create(path);
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("PathTooLongException");
+            }
+            catch (EndOfStreamException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("EndofStreaException");
+            }
+
+        }
+    }
+}
---
+++ ExceptionHandling/Testing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExceptionHandling;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\New Folder\text.txt";
+            string path2 = @"C:\New Folder\cdhgcydgyhgdhghhpppppppppppppppppppppppppppppppppppppppppppphhhhhhhhhhhhhhhhhhhhhllllppppppppppppppppppppppppppppppppplllllllllllllllllllllllllllhhhhhhhhkkklllllllllllllllllkkkkkkkkkkkkkkkkkkkkhhhggggyyyyyyyyyyyyyyyyyyyytttttttttttttttttttttttttt.txt";
+            HandlingInFiles.TxtReader(path1);
+            HandlingInFiles.TxtReader(path2);
+            for (int i = 0; i < 5; i++)
+                HandlingInFiles.TxtWriter(path1);
+        }
+    }
+}
