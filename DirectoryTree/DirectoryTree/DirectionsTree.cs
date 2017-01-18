b0a1bc864e382fb8e046862192f5beab6e8f694e
Dell <Dell@DESKTOP-QKEQKKO>
2017-01-18 18:09:38 +0400

    FoldersTree

---
+++ DirectoryTree/DirectoryTree/DirectionsTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryTree
+{
+    public class DirectionsTree
+    {
+        static ArrayList directories = new ArrayList();
+        static string newpath = String.Empty;
+        public static void GetTree(string path)
+        {
+
+            if (!IsContains(directories, path.Trim()))
+                directories.Add(path);
+            try
+            {
+                foreach (string d in Directory.GetDirectories(path))
+                {
+                    newpath = d;
+                    newpath = "   " + newpath;
+                    directories.Add(newpath);
+                    GetTree(newpath);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                directories.Add(ex.Message);
+            }
+
+        }
+
+        public static void Print()
+        {
+            foreach (var m in directories)
+                Console.WriteLine(m);
+        }
+        static bool IsContains(ArrayList list, string str)
+        {
+            bool t = false;
+            foreach (string m in list)
+                if (m.Contains(str))
+                {
+                    t = true;
+                    break;
+                }
+            return t;
+        }
+    }
+}
---
+++ DirectoryTree/Testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectoryTree;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectionsTree.GetTree(@"D:\");
+            DirectionsTree.Print();
+        }
+    }
+}
