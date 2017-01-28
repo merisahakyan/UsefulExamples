17fafeaf2ae3875c0060bea926963b502f11cec4
Dell <Dell@DESKTOP-QKEQKKO>
2017-01-28 21:33:03 +0400

    iterators

--- ImplementingIterates/ImplementingIterates/Derived.cs
+++ ImplementingIterates/ImplementingIterates/Derived.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ImplementingIterates
+namespace ImplementingIterators
 {
     public class Derived<T> : IEnumerable, IEnumerator
     {
