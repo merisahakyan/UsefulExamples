691a6b05fe56911cac8c030bf039e5f9c8ae8ce6
Dell <Dell@DESKTOP-QKEQKKO>
2017-01-28 21:16:22 +0400

    IEnumerable,IEnumerator interfaces

---
+++ CrazyButton/CrazyButton/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrazyButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point coordinates = Cursor.Position;
+            Random r = new Random();
+            int bx, by;
+            
+                bx = r.Next(0, 200);
+                by = r.Next(0, 250);
+                button1.Location = new Point(bx, by);
+            
+        }
+    }
+}
---
+++ ImplementingIterates/Testing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImplementingIterates;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>(10);
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            ints.Add(4);
+            ints.Add(5);
+            Derived<int> dint = new Derived<int>(ints);
+            for (int i = 0; i < ints.Count; i++)
+                if (dint.MoveNext())
+                    Console.WriteLine(dint.Current);
+                else
+                    dint.Reset();
+            dint.Reset();
+            Console.WriteLine("-------------");
+            for (int i = 0; i < ints.Count; i++)
+                if (dint.MoveNext())
+                    Console.WriteLine(dint.Current);
+                else
+                    dint.Reset();
+        }
+    }
+}
