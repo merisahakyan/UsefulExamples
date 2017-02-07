83bcac9e6e38355aa28030f89d5d2a41302d176f
merisahakyan <sahakyan_m@bk.ru>
2017-02-07 18:38:29 +0400

    TPL,async await

---
+++ AsyncronousProgramming/AsyncronousProgramming/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncronousProgramming
+{
+    class MyFirstAsync
+    {
+        public async void Print1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    Thread.Sleep(8);
+                    Console.WriteLine("aaaaaaaaaaa");
+                }
+            });
+            
+        }
+        public async void Print2()
+        {
+           await Task.Run(() =>
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    //Thread.Sleep(8);
+                    Console.WriteLine("bbb");
+                }
+            });
+            
+        }
+        public async void Print3()
+        {
+           await Task.Run(() =>
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    //Thread.Sleep(8);
+                    Console.WriteLine("ccccccc");
+                }
+            });
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFirstAsync ob = new MyFirstAsync();
+            ob.Print1();
+            ob.Print2();
+            ob.Print3();
+            for (int i = 0; i < 10000; i++)
+            {
+                Thread.Sleep(8);
+                Console.WriteLine("out");
+            }
+            Console.ReadLine();
+        }
+    }
+}
