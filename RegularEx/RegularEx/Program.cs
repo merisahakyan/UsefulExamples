11d9eb5a727fe9def47788f34b671f71b7476b9c
merisahakyan <sahakyan_m@bk.ru>
2017-03-07 18:22:23 +0400

    Regular Expressions

---
+++ RegularEx/RegularEx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegularEx
+{
+    class Program
+    {
+        static void OnlyDigits()
+        {   
+            //with length 4
+            string str;
+            Regex rg = new Regex("^\\d\\d\\d\\d$", RegexOptions.Compiled);
+            do
+            {
+                Console.WriteLine("Your input");
+                str = Console.ReadLine();
+            }
+            while (!rg.IsMatch(str));
+            Console.WriteLine("Great!");
+        }
+        static void DigitsAndSpaces()
+        {
+            //with lenth 4
+            string str;
+            Regex rg = new Regex("^\\d\\s*\\d\\s*\\d\\s*\\d\\s*?$", RegexOptions.Compiled);
+            do
+            {
+                Console.WriteLine("Your input");
+                str = Console.ReadLine();
+            }
+            while (!rg.IsMatch(str));
+            Console.WriteLine("Great!");
+        }
+        static void Main(string[] args)
+        {
+            OnlyDigits();
+            DigitsAndSpaces();
+        }
+    }
+}
