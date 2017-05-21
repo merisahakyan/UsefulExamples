b78e4c52ba969ba178db5bada0622b54cb1c4777
merisahakyan <sahakyan_m@bk.ru>
2017-05-21 20:34:06 +0400

    reading from excel file

---
+++ ReadingFromExcel/ReadingFromExcel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace ReadingFromExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Dell\Desktop\user.xlsx");
+            Microsoft.Office.Interop.Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            for (int i = 1; i <= 2; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                }
+            }
+        }
+    }
+}
