a9bfa08ca47e08786726891a7af6328c9d6f5523
merisahakyan <sahakyan_m@bk.ru>
2017-05-25 23:07:43 +0400

    sends great

--- ReadingFromExcel/SendingEmails/Program.cs
+++ ReadingFromExcel/SendingEmails/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Configuration;
@@ -15,27 +16,49 @@ namespace SendingEmails
 {
     class Program
     {
+        public static void SendMail(string to, string subject, string body)
+        {
+            Configuration oConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var mailSettings = oConfig.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+
+            if (mailSettings != null)
+            {
+                int port = mailSettings.Smtp.Network.Port;
+                string from = mailSettings.Smtp.From;
+                string host = mailSettings.Smtp.Network.Host;
+                string pwd = mailSettings.Smtp.Network.Password;
+                string uid = mailSettings.Smtp.Network.UserName;
+
+                var message = new MailMessage
+                {
+                    From = new MailAddress(@from)
+                };
+                message.To.Add(new MailAddress(to));
+                message.CC.Add(new MailAddress(from));
+                message.Subject = subject;
+                message.IsBodyHtml = true;
+                message.Body = body;
+
+                var client = new SmtpClient
+                {
+                    Host = host,
+                    Port = port,
+                    Credentials = new NetworkCredential(uid, pwd),
+                    EnableSsl = true
+                };
+
+                try
+                {
+                    client.Send(message);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            string sendto = "merisahakyan1@gmail.com";
-            MailMessage mailMsg = new MailMessage();
-            mailMsg.To.Add(new MailAddress(sendto));
-            mailMsg.Subject = "subject";
-
-            System.Configuration.Configuration config = WebConfigurationManager.
-                OpenWebConfiguration(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-            MailSettingsSectionGroup settings = (MailSettingsSectionGroup)config.GetSectionGroup("system.net/mailSettings");
-
-
-
-
-            mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString("put template's text", null, MediaTypeNames.Text.Plain));
-
-            SmtpClient smtpClient = new SmtpClient();
-            smtpClient.Credentials = new NetworkCredential(settings.Smtp.Network.UserName, settings.Smtp.Network.Password);
-
-            smtpClient.Send(mailMsg);
-            Console.WriteLine("sended");
+            SendMail("merisahakyan1@gmail.com", "sub", "hello");
         }
     }
 }
