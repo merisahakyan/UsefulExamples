4ecd0b0de8139db81ff54ee3b1170e1995ddb05f
merisahakyan <sahakyan_m@bk.ru>
2017-05-25 22:47:20 +0400

    sending configurations

--- ReadingFromExcel/SendingEmails/Program.cs
+++ ReadingFromExcel/SendingEmails/Program.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Configuration;
 using System.Net.Mail;
 using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+
+using System.Web.Configuration;
+
 
 namespace SendingEmails
 {
@@ -13,17 +17,23 @@ namespace SendingEmails
     {
         static void Main(string[] args)
         {
-            string sendto = "sahakyan_m@bk.ru";
+            string sendto = "merisahakyan1@gmail.com";
             MailMessage mailMsg = new MailMessage();
             mailMsg.To.Add(new MailAddress(sendto));
-            mailMsg.From = new MailAddress("forproj@ms.com");
             mailMsg.Subject = "subject";
-            //mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString("put template's text", null, MediaTypeNames.Text.Plain));
-            mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString("put template's text", null, MediaTypeNames.Text.Html));
+
+            System.Configuration.Configuration config = WebConfigurationManager.
+                OpenWebConfiguration(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            MailSettingsSectionGroup settings = (MailSettingsSectionGroup)config.GetSectionGroup("system.net/mailSettings");
+
+
+
+
+            mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString("put template's text", null, MediaTypeNames.Text.Plain));
+
             SmtpClient smtpClient = new SmtpClient();
-            smtpClient.Credentials = CredentialCache.DefaultNetworkCredentials;
-            //System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("sahakyan_meri", "p42Zmx39");
-            //smtpClient.Credentials = credentials;
+            smtpClient.Credentials = new NetworkCredential(settings.Smtp.Network.UserName, settings.Smtp.Network.Password);
+
             smtpClient.Send(mailMsg);
             Console.WriteLine("sended");
         }
