772b0d83130d397c63a43e54c3a67491634a531c
merisahakyan <sahakyan_m@bk.ru>
2017-06-25 14:53:34 +0400

    testing inheritance virtual,override,new

---
+++ StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMembers
+{
+    public class A
+    {
+        public virtual void AAA()
+        {
+            Console.WriteLine("class a");
+        }
+    }
+    public class B:A
+    {
+        public override void AAA()
+        {
+            Console.WriteLine("class b");
+        }
+    }
+    public class C : B
+    {
+        public new void AAA()
+        {
+            Console.WriteLine("class c");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            A b = new B();
+            A c = new C();
+            a.AAA();
+            b.AAA();
+            c.AAA();
+            Console.ReadLine();
+        }
+    }
+}
