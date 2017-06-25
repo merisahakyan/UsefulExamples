758538aef5027472a56e374761643036b933ecdc
merisahakyan <sahakyan_m@bk.ru>
2017-06-25 16:55:26 +0400

    .

--- StaticMembers/StaticMembers/Program.cs
+++ StaticMembers/StaticMembers/Program.cs
@@ -12,6 +12,7 @@ namespace StaticMembers
         {
             Console.WriteLine("class a");
         }
+        
     }
     public class B:A
     {
@@ -27,17 +28,46 @@ namespace StaticMembers
             Console.WriteLine("class c");
         }
     }
+    class Box
+    {
+        static Box()
+        {
+            Console.WriteLine("static ctor was called");
+        }
+
+        public Box()
+        {
+            Console.WriteLine("default ctor was calleds");
+        }
+
+        public static string Data { get { return "You are trying to get data"; } }
+
+        public const int Foo = 7;
+    }
+    interface IA
+    {
+        
+    }
+    struct SA:IA
+    {
+
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            A a = new A();
-            A b = new B();
-            A c = new C();
-            a.AAA();
-            b.AAA();
-            c.AAA();
+            //A a = new A();
+            //A b = new B();
+            //A c = new C();
+            //a.AAA();
+            //b.AAA();
+            //c.AAA();
+            //var data = Box.Data;
+            //var box = new Box();
+            //var data2 = Box.Data;
+            //var box2 = new Box();
+            Console.WriteLine(3+"7"+6+1);
             Console.ReadLine();
         }
     }
