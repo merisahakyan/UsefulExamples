507a2d5da4d401328578d5884ee74dee64ed5392
merisahakyan <sahakyan_m@bk.ru>
2017-10-02 09:55:35 +0400

    Objects To Csv format

---
+++ ObjectToFile/ObjectToFile/CSVProvider.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectToFile
+{
+    public class CSVProvider
+    {
+        public static void GetHeaders<T>(ref StringBuilder sb, T @object, string propName = null)
+        {
+            var propertyInfoArray = typeof(T).GetProperties().ToList();
+            var properties = propertyInfoArray.Select(x => x.Name).ToList();
+            foreach (var prop in propertyInfoArray)
+            {
+
+                if (prop.GetType().IsClass && !prop.PropertyType.FullName.StartsWith("System."))
+                {
+                    var obj = @object.GetType().GetProperty(prop.Name).GetValue(@object);
+                    MethodInfo method = typeof(CSVProvider).GetMethod("GetHeaders");
+                    MethodInfo generic = method.MakeGenericMethod(obj.GetType());
+                    generic.Invoke(null, new object[] { sb, obj, prop.Name + "." });
+
+                }
+                else
+                {
+                    sb.Append(propName + prop.Name);
+                    sb.Append(",");
+                }
+            }
+            sb.Append(Environment.NewLine);
+        }
+        public static void GetValues<T>(ref StringBuilder sb, T @object)
+        {
+            var propertyInfoArray = typeof(T).GetProperties().ToList();
+            var properties = propertyInfoArray.Select(x => x.Name).ToList();
+            foreach (var value in properties)
+            {
+
+                PropertyInfo prop = @object.GetType().GetProperty(value);
+
+                if (prop.PropertyType.GetInterface("IEnumerable") != null && prop.PropertyType != typeof(string))
+                {
+
+                    IEnumerable collection = prop.GetValue(@object) as IEnumerable;
+                    foreach (var listItem in collection)
+                    {
+                        if (listItem.GetType().IsClass && !listItem.GetType().FullName.StartsWith("System."))
+                        {
+                            MethodInfo method = typeof(CSVProvider).GetMethod("GetValuesFromList");
+                            MethodInfo generic = method.MakeGenericMethod(listItem.GetType());
+                            generic.Invoke(null, new object[] { sb, listItem });
+                        }
+                        else
+                        {
+                            sb.Append(listItem);
+                            sb.Append(";");
+                        }
+                    }
+                    sb.Remove(sb.Length - 1, 1);
+                    sb.Append(",");
+                }
+                else
+                {
+                    if (prop.GetType().IsClass && !prop.PropertyType.FullName.StartsWith("System."))
+                    {
+                        var obj = @object.GetType().GetProperty(prop.Name).GetValue(@object);
+                        MethodInfo method = typeof(CSVProvider).GetMethod("GetValues");
+                        MethodInfo generic = method.MakeGenericMethod(obj.GetType());
+                        generic.Invoke(null, new object[] { sb, obj });
+                    }
+                    else
+                    {
+                        sb.Append(prop.GetValue(@object, null));
+                        sb.Append(",");
+                    }
+
+                }
+            }
+        }
+
+        public static void GetValuesFromList<T>(ref StringBuilder sb, T @object)
+        {
+            if (@object.GetType().IsClass && !@object.GetType().FullName.StartsWith("System."))
+            {
+                StringBuilder newCell = new StringBuilder();
+                newCell.Append("{ ");
+                var propertyInfoArray = typeof(T).GetProperties().ToList();
+                var properties = propertyInfoArray.Select(x => x.Name).ToList();
+                foreach (var value in properties)
+                {
+                    PropertyInfo prop = @object.GetType().GetProperty(value);
+                    newCell.Append(prop.Name + ":" + prop.GetValue(@object, null));
+                    newCell.Append(" ; ");
+                }
+                newCell.Append(" };");
+                sb.Append(newCell);
+            }
+            else
+            {
+                sb.Append(@object.ToString());
+                sb.Append(",");
+            }
+        }
+
+        public static void CreateStringForCSV<T>(ref StringBuilder sb, T @object)
+        {
+            var propertyInfoArray = typeof(T).GetProperties().ToList();
+            var properties = propertyInfoArray.Select(x => x.Name).ToList();
+            if (properties == null)
+                throw new Exception("Properties count is 0.");
+            GetHeaders<T>(ref sb, @object);
+            GetValues<T>(ref sb, @object);
+        }
+        public static void CreateStringForCSVGenericCollection<T>(ref StringBuilder sb, IEnumerable<T> collection)
+        {
+            if (ReferenceEquals(collection, null))
+                throw new NullReferenceException();
+
+
+            if (collection.First().GetType().IsClass && !collection.First().GetType().FullName.StartsWith("System."))
+            {
+                GetHeaders<T>(ref sb, collection.First());
+            }
+            foreach (var item in collection)
+            {
+                if (item.GetType().IsClass && !item.GetType().FullName.StartsWith("System."))
+                {
+                    MethodInfo method = typeof(CSVProvider).GetMethod("GetValues");
+                    MethodInfo generic = method.MakeGenericMethod(item.GetType());
+                    generic.Invoke(null, new object[] { sb, item });
+                    sb.Append(Environment.NewLine);
+                }
+                else
+                {
+                    sb.Append(item.ToString());
+                    sb.Append(Environment.NewLine);
+                }
+
+            }
+        }
+        public static void CreateStringForCSVCollection(ref StringBuilder sb, IEnumerable collection)
+        {
+            if (ReferenceEquals(collection, null))
+                throw new NullReferenceException();
+            foreach (var item in collection)
+            {
+                if (item.GetType().IsClass && !item.GetType().FullName.StartsWith("System."))
+                {
+                    MethodInfo method2 = typeof(CSVProvider).GetMethod("GetValues");
+                    MethodInfo generic2 = method2.MakeGenericMethod(item.GetType());
+                    generic2.Invoke(null, new object[] { sb, item });
+                    sb.Append(Environment.NewLine);
+                }
+                else
+                {
+                    sb.Append(item.ToString());
+                    sb.Append(Environment.NewLine);
+                }
+
+            }
+        }
+    }
+}
---
+++ ObjectToFile/ObjectToFile/ToFile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectToFile
+{
+    public static class ToFile
+    {
+        public static void ToCsv<T>(this T @object, string path = null)
+        {
+            if (ReferenceEquals(@object, null))
+            {
+                throw new NullReferenceException();
+            }
+            if (ReferenceEquals(path, null))
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            path += $"\\{System.DateTime.Now.ToShortDateString().Replace('/', '-')}_table.csv";
+
+            StringBuilder sb = new StringBuilder();
+
+            if (@object.GetType().GetInterface("IEnumerable") == null || @object.GetType() == typeof(string))
+            {
+                if (@object.GetType().IsClass)
+                {
+                    CSVProvider.CreateStringForCSV<T>(ref sb, @object);
+                    File.WriteAllText(path, sb.ToString());
+
+                }
+                else
+                {
+                    File.WriteAllText(path, @object.ToString());
+                }
+            }
+            else
+            {
+                var type = @object.GetType();
+                if (type.IsGenericType)
+                {
+                    MethodInfo method = typeof(CSVProvider).GetMethod("CreateStringForCSVGenericCollection");
+                    MethodInfo generic = method.MakeGenericMethod(type.GetGenericArguments()[0]);
+                    generic.Invoke(null, new object[] { sb, @object });
+                    File.WriteAllText(path, sb.ToString());
+                }
+                else
+                {
+                    MethodInfo method = typeof(CSVProvider).GetMethod("CreateStringForCSVCollection");
+                    method.Invoke(null, new object[] { sb, @object });
+                    File.WriteAllText(path, sb.ToString());
+                }
+            }
+
+        }
+
+        public static void ToCsv<T>(this IEnumerable<T> source, string path = null)
+        {
+            if (ReferenceEquals(source, null))
+            {
+                throw new NullReferenceException();
+            }
+            if (ReferenceEquals(path, null))
+            {
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            path += $"\\{System.DateTime.Now.ToShortDateString().Replace('/', '-')}_table.csv";
+
+            StringBuilder sb = new StringBuilder();
+            CSVProvider.CreateStringForCSVGenericCollection<T>(ref sb, source);
+            File.WriteAllText(path, sb.ToString());
+        }
+    }
+}
